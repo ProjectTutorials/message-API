cf8baed2842886276193e4ab0e73b265f84bbaaa
Prãt!k Kabade <pratikkabade@hotmail.com>
2022-11-03 16:15:56 +0530

    Short but Complicated

--- Routes/Message.js
+++ Routes/Message.js
@@ -1,27 +1,62 @@
 const express = require("express");
-const {
-  GetAllMessage,
-  GetOneMessage,
-  CreateMessage,
-  UpdateMessage,
-  DeleteMessage,
-} = require("../Controllers/MessageController");
 const router = express.Router();
 const Messages = require("../Models/Message");
 
 // GET ALL
-router.get("/", GetAllMessage);
+router.get("/", async (req, res) => {
+  try {
+    const Message = await Messages.find();
+    res.status(200).json(Message);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
 
 // GET ONE
-router.get("/:id", GetOneMessage);
+router.get("/:id", async (req, res) => {
+  try {
+    const Message = await Messages.findById(req.params.id);
+    res.status(200).json(Message);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
 
 //CREATE
-router.post("/", CreateMessage);
+router.post("/", async (req, res) => {
+  const Message = new Messages({
+    name: req.body.name,
+    message: req.body.message,
+    date: req.body.date,
+  });
+  try {
+    const newMessage = await Message.save();
+    res.status(201).json(newMessage);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
 
 //UPDATE
-router.put("/:id", UpdateMessage);
+router.put("/:id", async (req, res) => {
+  try {
+    const Message = await Messages.findById(req.params.id);
+    await Message.updateOne({ $set: req.body });
+    res.status(200).send("Message Updated");
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
 
 //DELETE
-router.delete("/:id", DeleteMessage);
+router.delete("/:id", async (req, res) => {
+  try {
+    const Message = await Messages.findById(req.params.id);
+    await Message.remove();
+    res.status(200).send("Message Deleted");
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
 
 module.exports = router;
--- index.js
+++ index.js
@@ -9,7 +9,18 @@ const Port = process.env.PORT || 5000;
 app.listen(Port, () => console.log(`Server started at port no ${Port}`));
 
 // Database Connection
-require("./Database/Connection");
+// Requirements
+const mongoose = require("mongoose");
+
+// Connect to MongoDB
+mongoose.connect(process.env.DB_URL, { useNewUrlParser: true });
+const db = mongoose.connection;
+
+// CONNECTION LOG
+db.once("open", () => console.log("Database Connected!"));
+
+// ERROR LOG
+db.on("error", (error) => console.error(error));
 
 // Routing
 const messageRouter = require("./Routes/Message");
